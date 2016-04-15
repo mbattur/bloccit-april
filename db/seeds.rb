9bcfcbacb103cdf2a09850dda86d0f19c1c92ccf
mbattur <mbattur@yahoo.com>
2016-04-15 00:04:27 +0000

    34 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,24 @@
 require 'random_data'
 
  5.times do
+   Topic.create!(
+     name:         RandomData.random_sentence,
+     description:  RandomData.random_paragraph
+   )
+ end
+ topics = Topic.all
+ 
+ 15.times do
 
    Post.create!(
-
+     topic:  topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
  end
  posts = Post.all
 
- 10.times do
+ 30.times do
    Comment.create!(
      post: posts.sample,
      body: RandomData.random_paragraph
@@ -18,5 +26,6 @@ require 'random_data'
  end
  
  puts "Seed finished"
+ puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
